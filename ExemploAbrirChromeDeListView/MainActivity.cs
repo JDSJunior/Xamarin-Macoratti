2df1a5219739ae967ab5ceffec15f68dc1b0f279
Júnior Silva <junior_dariana@hotmail.com>
2017-05-04 12:15:31 -0300

    2° commit

---
+++ ExemploAbrirChromeDeListView/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Views;
+using Android.Content;
+
+namespace ExemploAbrirChromeDeListView
+{
+    [Activity(Label = "ExemploAbrirChromeDeListView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : ListActivity
+    {
+        //lista para usar como BD
+        List<string> recursos;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+
+            //popula a lista
+            CarregarList();
+
+            //insere os dados na listview
+            ListAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, recursos);
+
+        }
+
+        //sobreescreve o metodo de clique na list
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            //seleciona o item clicado
+            var item = recursos[position];
+
+            Toast.MakeText(this, item, ToastLength.Short).Show();
+
+            //abrir o navegador
+            var uri = Android.Net.Uri.Parse("https://www.google.com/#q=" + item);
+
+            //define uma intent
+            var intent = new Intent(Intent.ActionView, uri);
+
+            //inicia a intent
+            StartActivity(intent);
+        }
+
+        //metodo que popula a lista
+        private void CarregarList()
+        {
+            recursos = new List<string>();
+            recursos.Add("Visual C#");
+            recursos.Add("Visual Basic");
+            recursos.Add("VB .NET");
+            recursos.Add("JavaScript");
+        }
+
+        
+    }
+}
+
---
+++ ExemploAutoCompleteTextView/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ExemploAutoCompleteTextView
+{
+    [Activity(Label = "ExemploAutoCompleteTextView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        string[] estados;
+        AutoCompleteTextView autoCompletar1;
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            estados = new string[] { "Acre", "Alagoas","Amapá", "Amazonas",
+                                     "Bahia", "Ceará", "Distrito Federal",
+                                     "Espírito Santo", "Goiás", "Maranhão",
+                                     "Mato Grosso", "Mato Grosso do Sul",
+                                     "Minas Gerais", "Pará", "Paraíba",
+                                     "Paraná", "Pernambuco", "Piauí",
+                                     "Rio de Janeiro", "Rio Grande do Norte",
+                                     "Rio Grande do Sul", "Rondônia", "Roraima",
+                                     "Santa Catarina", "São Paulo", "Sergipe",
+                                     "Tocantins" };
+
+            autoCompletar1 = FindViewById<AutoCompleteTextView>(Resource.Id.autoCompleteTextView1);
+            autoCompletar1.Threshold = 3;
+
+            //adpter do tipo string para a insercao no AutoCompleteTextView 
+            //parametros
+            //contexto onde o adapter deve estar, layout do android que deve mostrar os dados na tela, origem dos dados
+            ArrayAdapter adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, estados);
+
+            autoCompletar1.Adapter = adapter;
+
+            Button btnEnviar = FindViewById<Button>(Resource.Id.btnEnviar);
+
+            btnEnviar.Click += delegate
+            {
+
+                if(autoCompletar1.Text != string.Empty)
+                {
+                    Toast.MakeText(this, "Estado: " + autoCompletar1.Text, ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Selecione um Estado", ToastLength.Short).Show();
+                }
+
+            };
+
+
+
+        }
+    }
+}
+
